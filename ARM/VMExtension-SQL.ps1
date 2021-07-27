cb5983e28c9f8e5498e7ffd735a55039c6b0ed2d
Cristopher Robles Ríos <crisrc012@hotmail.com>
2021-07-26 21:28:27 -0600

    Update VMExtension-SQL.ps1
    Validación de tiempo

--- ARM/VMExtension-SQL.ps1
+++ ARM/VMExtension-SQL.ps1
@@ -1,14 +1,27 @@
 # Se establece el protocolo TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Se establece la preferencia en error
+$ErrorActionPreference = 'SilentlyContinue'
 
 # Se descarga el Script de SQL
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/crisrc012/TekNite/main/ARM/SmartHotel/scriptSmartHotel.sql" -OutFile C:\scriptSmartHotel.sql
 
-# Se habilitar el modo de Login Mixto
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQLServer" -Name "LoginMode" -Value 2 -Force
+# Se usa para validar que esté disponible el SQL
+while($true) {
+    Start-Sleep -Seconds 3
+    if(Get-Service -Name MSSQLSERVER) {
+        Start-Sleep -Seconds 60
+        Get-Service -Name MSSQLSERVER
+        # Se habilitar el modo de Login Mixto
+        Set-ItemProperty -Path "HKLM:\Software\Microsoft\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQLServer" -Name "LoginMode" -Value 2 -Force
 
-# Se ejecuta el Script
-Invoke-Sqlcmd -ServerInstance $env:ComputerName -InputFile C:\scriptSmartHotel.sql
+        # Se ejecuta el Script
+        Invoke-Sqlcmd -ServerInstance $env:ComputerName -InputFile C:\scriptSmartHotel.sql
 
-# Se reinicia el servicio
-Restart-Service -Name MSSQLSERVER -Force
+        # Se reinicia el servicio
+        Restart-Service -Name MSSQLSERVER -Force
+        
+        # Una vez completado se sale sin error
+        return 0
+    }
+}

32c10ac3f112f3325ec2b1804fa4fc3aa32207ac
Cristopher Robles Ríos <crisrc012@hotmail.com>
2021-07-26 20:28:16 -0600

    Update VMExtension-Web.ps1

--- ARM/VMExtension-Web.ps1
+++ ARM/VMExtension-Web.ps1
@@ -1,13 +1,16 @@
 # Instalar Dependencias
 Get-WindowsFeature -Name "Web-*" | Install-WindowsFeature
 
+# Se elimina el sitio por defecto
+Remove-Website -Name "Default Web Site" 
 
+# Descargar los recursos
+Invoke-WebRequest -Uri https://github.com/crisrc012/TekNite/archive/refs/heads/main.zip -OutFile C:\main.zip
+# Descomprimir recursos
+Expand-Archive -LiteralPath C:\main.zip -DestinationPath C:\
+# Mover el contenido de la app y Wcf
+Move-Item C:\TekNite-main\ARM\SmartHotel\SmartHotel.* -Destination C:\inetpub\
 
-# Crear Sitio
-#Remove-Item -Path C:\inetpub\wwwroot\* -Recurse -Force
-# Descargar el sitio clon de conzultek.com/contactenos
-#Invoke-WebRequest -Uri https://github.com/crisrc012/TekNite/archive/main.zip -OutFile C:\main.zip
-# Descomprimir el sitio
-#Expand-Archive -LiteralPath C:\main.zip -DestinationPath C:\
-# Mover el contenido del sitio a la raiz del sitio por defecto de IIS
-#Move-Item C:\TekNite-main\site\wvw.conzultek.com\* -Destination C:\inetpub\wwwroot\
+# Crear nuevos sitios
+New-Website -Name "SmartHotel.Registration" -Port 80 -PhysicalPath "C:\inetpub\SmartHotel.Registration"
+New-Website -Name "SmartHotel.Registration.Wcf" -Port 2901 -PhysicalPath "C:\inetpub\SmartHotel.Registration.Wcf"
